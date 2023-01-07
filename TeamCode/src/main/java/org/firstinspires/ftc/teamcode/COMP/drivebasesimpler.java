42a057f2884e092fab31966e2246b3753d3f8411
mzf11125 <mzidanfatonie@gmail.com>
2023-01-07 15:20:12 -0800

    Create drivebasesimpler.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/COMP/drivebasesimpler.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode.COMP;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@teleop
+public class drivebasesimpler extends LinearOpMode {
+
+    DcMotor motorFrontLeft;
+    DcMotor motorBackLeft;
+    DcMotor motorFrontRight;
+    DcMotor motorBackRight;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Initialize motors
+        motorFrontLeft = hardwareMap.dcMotor.get("frontLeft");
+        motorBackLeft = hardwareMap.dcMotor.get("backLeft");
+        motorFrontRight = hardwareMap.dcMotor.get("frontRight");
+        motorBackRight = hardwareMap.dcMotor.get("backRight");
+
+        // Reverse right motors
+        motorFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            // Set motor powers using gamepad input
+            motorFrontLeft.setPower(-gamepad1.left_stick_y - gamepad1.left_stick_x - gamepad1.right_stick_x);
+            motorBackLeft.setPower(-gamepad1.left_stick_y + gamepad1.left_stick_x - gamepad1.right_stick_x);
+            motorFrontRight.setPower(-gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x);
+            motorBackRight.setPower(-gamepad1.left_stick_y - gamepad1.left_stick_x + gamepad1.right_stick_x);
+        }
+    }
+}
